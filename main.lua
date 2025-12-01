928b4586b0b18cffb035150250cd58e8c6f9542e
ymkeee <forfunpaychilled@gmail.com>
2025-12-01 13:39:27 +0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -4,4 +4,6 @@ if placeId == 108447089422962 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/ymkeee/sierra/refs/heads/main/game1.lua"))()
 elseif placeId == 129864072130807 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/ymkeee/sierra/refs/heads/main/game2.lua"))()
+elseif placeId == 129854327403392 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/ymkeee/sierra/refs/heads/main/game3.lua"))()
 end
